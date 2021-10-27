3355020ee544f2c541e212d30743309c9f2701ff
Trevor Le <32115579+trvrle@users.noreply.github.com>
2021-10-27 15:37:06 -0600

    Added FoodItem and Menu Item

---
+++ Spaghetti Coders/Controls/FoodItem.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spaghetti_Coders.Controls
+{
+    /// <summary>
+    /// Interaction logic for FoodItem.xaml
+    /// </summary>
+    public partial class FoodItem : UserControl
+    {
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register( "Title", typeof( string ), typeof( FoodItem ), new PropertyMetadata( string.Empty ) );
+
+        public string Title
+        {
+            get { return (string)GetValue( TitleProperty ); }
+            set { SetValue( TitleProperty, value ); }
+        }
+
+        public static readonly RoutedEvent ClickEvent =
+            EventManager.RegisterRoutedEvent( nameof(Click), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof( FoodItem ) );
+
+        public event RoutedEventHandler Click
+        {
+            add { AddHandler( ClickEvent, value ); }
+            remove { RemoveHandler( ClickEvent, value ); }
+        }
+        public FoodItem()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent( new RoutedEventArgs( ClickEvent ) );
+        }
+    }
+}
---
+++ Spaghetti Coders/Controls/Menu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spaghetti_Coders.Controls
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : UserControl
+    {
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register( "Title", typeof( string ), typeof( Menu ), new PropertyMetadata( string.Empty ) );
+        
+        public string Title
+        {
+            get { return (string)GetValue( TitleProperty ); }
+            set { SetValue( TitleProperty, value ); }
+        }
+
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFoodItemClick( object sender, RoutedEventArgs e )
+        {
+            MessageBox.Show( "Clicked Food Item", "Success", MessageBoxButton.OK, MessageBoxImage.Information );
+        }
+    }
+}
