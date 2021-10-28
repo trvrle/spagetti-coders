710b19491c453678a7f30b7edecfd46282766d00
Trevor Le <32115579+trvrle@users.noreply.github.com>
2021-10-28 16:58:32 -0600

    Added search bar and moved images

---
+++ Spaghetti Coders/Controls/SearchBox.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spaghetti_Coders.Controls
+{
+    /// <summary>
+    /// Interaction logic for SearchBox.xaml
+    /// </summary>
+    public partial class SearchBox : UserControl
+    {
+        public SearchBox()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSearch( object sender, MouseButtonEventArgs e )
+        {
+            MessageBox.Show( "Clicked Search", "Search", MessageBoxButton.OK, MessageBoxImage.Information );
+        }
+    }
+}
