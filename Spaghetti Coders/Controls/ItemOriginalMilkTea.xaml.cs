0beb15864b54dd62ca7c85c5b33d59d4934c14d1
Yihan <47429231+yihannwei@users.noreply.github.com>
2021-11-17 19:14:16 -0700

    Create ItemOriginalMilkTea.xaml.cs

---
+++ Spaghetti Coders/Controls/ItemOriginalMilkTea.xaml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spaghetti_Coders.Controls
+{
+    /// <summary>
+    /// Interaction logic for ItemOriginalMilkTea.xaml
+    /// </summary>
+    public partial class ItemOriginalMilkTea : UserControl
+    {
+        public ItemOriginalMilkTea()
+        {
+            InitializeComponent();
+        }
+    }
+}
