f31ec651be0bb493e57475805a72ab4759efc5a1
Yihan <47429231+yihannwei@users.noreply.github.com>
2021-11-17 18:25:16 -0700

    Create ItemTempura.xaml.cs

---
+++ Spaghetti Coders/Controls/ItemTempura.xaml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spaghetti_Coders.Controls
+{
+    /// <summary>
+    /// Interaction logic for ItemTempura.xaml
+    /// </summary>
+    public partial class ItemTempura : UserControl
+    {
+        public ItemTempura()
+        {
+            InitializeComponent();
+        }
+    }
+}
